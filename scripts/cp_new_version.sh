3d406e73d2f091368cc943b1ce06a659a756e8ea
MartenPostma <martenp@gmail.com>
2020-09-23 22:00:02 +0200

    updated v0.1

---
+++ scripts/cp_new_version.sh
@@ -0,0 +1,9 @@
+export folder=/Users/marten/PycharmProjects/v0.1/
+
+cd ..
+cp -r $folder/frame .
+cp -r $folder/lu .
+cp $folder/luIndex.xml .
+cp $folder/frameIndex.xml .
+
+
---
+++ scripts/git_rm.sh
@@ -0,0 +1,5 @@
+cd ..
+git rm luIndex.xml
+git rm frameIndex.xml
+git rm -r frame
+git rm -r lu
